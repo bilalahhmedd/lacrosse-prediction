2c9204c9977d16be1a1c17bb4b5bc52995f9148d
bilal ahmed <bilal.ahhmedd@gmail.com>
2024-04-20 14:16:34 +0500

    feat(etl): Adding ETL code by bilalahmed

---
+++ etl/column_maps.py
@@ -0,0 +1,36 @@
+""" module contains column mapping dictionaries """
+
+american_select_names_map_dict = {
+    'Region':'state',
+    'School': 'college',
+    'CLUB TEAM':'club',
+    }
+
+clublacrose_names_map_dict = {
+    'Player Name':'name',
+    'University':'college',
+    'Division': 'commitment_year',
+    }
+
+laxnumbers_names_map_dict = {
+    'Name':'name',
+    'State': 'state',
+    'Club': 'club',
+    'Previous  School':'high_school',
+    'Class':'commitment_year',
+    'Pos':'position',
+    'Div':'division'
+    }
+
+toplax_names_map_dict = {
+    'High School Name':'high_school',
+    'Grad Year':'commitment_year',
+    'Graduation Year':'commitment_year',
+    'region':'state',
+    'ST':'state',
+    'COLLEGE COMMITTED TO': 'college',
+    'College Committed To/Signed With':'college',
+    'College Committed to':'college',
+    'Club Team':'club',
+    'DIV':'division'
+    }
